13e91650e154b658692ec0f5145e10dd6deb1ee5
Bruno Marques <blmarques.dev@gmail.com>
2022-05-18 02:00:16 -0300

    style(lint-fix): fix lint trash code

--- src/useCases/errorHandler/errorHandler.ts
+++ src/useCases/errorHandler/errorHandler.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
+// import { Request, Response } from "express";
 
-export default class ErrorHandler {
-  public exeptionHandler(error: Error, _req: Request, res: Response) {
-    console.log(error);
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-}
+// export default class ErrorHandler {
+//   public exeptionHandler(error: Error, _req: Request, res: Response) {
+//     console.log(error);
+//     res.status(500).json({
+//       message: error.message,
+//     });
+//   }
+// }
--- src/useCases/orderCases/orderServices.ts
+++ src/useCases/orderCases/orderServices.ts
@@ -1,11 +1,13 @@
 import OrdersModel from '../../models/ordersModel';
+import orderFormateResponse from './orderUtilSanityResponseQuery';
+import { IOrderRepository } from './repository/repository';
 
 export default class GetOrderService {
   public orderModel = new OrdersModel();
 
   public async execute() {
     const order = await this.orderModel.getAllOrders();
-
-    return order;
+    const orderFormat = orderFormateResponse(order as IOrderRepository[]);
+    return orderFormat;
   }
 }
